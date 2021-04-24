f00d139fbdf53765f094ad8078d97ae30ea60c49
MarcoFavorito <marco.favorito@gmail.com>
2021-04-24 19:44:59 +0200

    update release script
    rename output directory with:
    
       mona_${version}_${platform}-${arch}

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,47 +1,64 @@
 #!/usr/bin/env bash
 
-./configure && make && sudo make install-strip
 
-rm -rf release
-mkdir release
-cp README release/
-cp COPYING release/
+PLATFORM="linux"
+ARCH="amd64"
+VERSION="1.4-19.dev0"
+RELEASE_NAME="mona_${VERSION}_${PLATFORM}-${ARCH}"
+prefix="/usr/local"
+includedir="${prefix}/include/"
 
-mkdir release/bin
-cp Front/mona release/bin
+OUTPUT_DIR="${RELEASE_NAME}"
+OUTPUT_TAR="${RELEASE_NAME}.tar.gz"
 
-mkdir release/lib
-cp -P GTA/.libs/libmonagta.a release/lib   
-cp -P GTA/.libs/libmonagta.so release/lib   
-cp -P GTA/.libs/libmonagta.so.1 release/lib   
-cp -P GTA/.libs/libmonagta.so.1.0.4 release/lib   
-cp -P DFA/.libs/libmonadfa.a release/lib   
-cp -P DFA/.libs/libmonadfa.so release/lib   
-cp -P DFA/.libs/libmonadfa.so.1 release/lib   
-cp -P DFA/.libs/libmonadfa.so.1.0.4 release/lib   
-cp -P BDD/.libs/libmonabdd.a release/lib   
-cp -P BDD/.libs/libmonabdd.so release/lib   
-cp -P BDD/.libs/libmonabdd.so.1 release/lib   
-cp -P BDD/.libs/libmonabdd.so.1.0.4 release/lib   
-cp -P Mem/.libs/libmonamem.a release/lib   
-cp -P Mem/.libs/libmonamem.so release/lib   
-cp -P Mem/.libs/libmonamem.so.1 release/lib   
-cp -P Mem/.libs/libmonamem.so.1.0.4 release/lib   
-for i in release/lib/*;
+./configure && make -j4
+
+rm -rf "${OUTPUT_DIR}"
+mkdir "${OUTPUT_DIR}"
+cp README "${OUTPUT_DIR}"
+cp COPYING  "${OUTPUT_DIR}"
+
+mkdir ${OUTPUT_DIR}/bin
+cp Front/.libs/mona ${OUTPUT_DIR}/bin
+
+mkdir ${OUTPUT_DIR}/lib
+cp -P GTA/.libs/libmonagta.a ${OUTPUT_DIR}/lib   
+cp -P GTA/.libs/libmonagta.so ${OUTPUT_DIR}/lib   
+cp -P GTA/.libs/libmonagta.so.1 ${OUTPUT_DIR}/lib   
+cp -P GTA/.libs/libmonagta.so.1.0.4 ${OUTPUT_DIR}/lib   
+cp -P DFA/.libs/libmonadfa.a ${OUTPUT_DIR}/lib   
+cp -P DFA/.libs/libmonadfa.so ${OUTPUT_DIR}/lib   
+cp -P DFA/.libs/libmonadfa.so.1 ${OUTPUT_DIR}/lib   
+cp -P DFA/.libs/libmonadfa.so.1.0.4 ${OUTPUT_DIR}/lib   
+cp -P BDD/.libs/libmonabdd.a ${OUTPUT_DIR}/lib   
+cp -P BDD/.libs/libmonabdd.so ${OUTPUT_DIR}/lib   
+cp -P BDD/.libs/libmonabdd.so.1 ${OUTPUT_DIR}/lib   
+cp -P BDD/.libs/libmonabdd.so.1.0.4 ${OUTPUT_DIR}/lib   
+cp -P Mem/.libs/libmonamem.a ${OUTPUT_DIR}/lib   
+cp -P Mem/.libs/libmonamem.so ${OUTPUT_DIR}/lib   
+cp -P Mem/.libs/libmonamem.so.1 ${OUTPUT_DIR}/lib   
+cp -P Mem/.libs/libmonamem.so.1.0.4 ${OUTPUT_DIR}/lib   
+for i in ${OUTPUT_DIR}/lib/*;
 do
     strip $i;
 done
 
-mkdir release/include
-mkdir release/include/mona
-cp Mem/mem.h release/include/mona
-cp Mem/gnuc.h release/include/mona
-cp Mem/dlmalloc.h release/include/mona
-cp BDD/bdd_external.h release/include/mona
-cp BDD/bdd_dump.h release/include/mona
-cp BDD/bdd_internal.h release/include/mona
-cp BDD/bdd.h release/include/mona
-cp BDD/hash.h release/include/mona
-cp DFA/dfa.h release/include/mona
-cp GTA/gta.h release/include/mona
-cp config.h release/include/mona
+mkdir ${OUTPUT_DIR}/include
+mkdir ${OUTPUT_DIR}/include/mona
+cp Mem/mem.h ${OUTPUT_DIR}/include/mona
+cp Mem/gnuc.h ${OUTPUT_DIR}/include/mona
+cp Mem/dlmalloc.h ${OUTPUT_DIR}/include/mona
+cp BDD/bdd_external.h ${OUTPUT_DIR}/include/mona
+cp BDD/bdd_dump.h ${OUTPUT_DIR}/include/mona
+cp BDD/bdd_internal.h ${OUTPUT_DIR}/include/mona
+cp BDD/bdd.h ${OUTPUT_DIR}/include/mona
+cp BDD/hash.h ${OUTPUT_DIR}/include/mona
+cp DFA/dfa.h ${OUTPUT_DIR}/include/mona
+cp GTA/gta.h ${OUTPUT_DIR}/include/mona
+cp config.h ${OUTPUT_DIR}/include/mona
+
+tar -c "${OUTPUT_DIR}" -f "${OUTPUT_TAR}"
+rm -r "${OUTPUT_DIR}"
+
+echo "Output in ${OUTPUT_TAR}"
+
